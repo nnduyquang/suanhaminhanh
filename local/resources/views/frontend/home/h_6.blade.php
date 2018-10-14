f5e2507cde40716c7d88274c6370bec8d46b12ad
Quang-PC <nnduyquang@gmail.com>
2018-10-15 06:40:16 +0700

    ok

--- local/resources/views/frontend/home/h_6.blade.php
+++ local/resources/views/frontend/home/h_6.blade.php
@@ -9,96 +9,96 @@
                 {{--<span>Thông tin báo giá dịch vụ sửa chửa nhà và các dịch vụ liên quan.</span>--}}
             </div>
 
-            <div class="col-md-3 text-center">
-                <div class="bao-gia border">
-                    <h4>THIẾT KẾ</h4>
-                    <h3>MIỄN PHÍ<br>
-                        <span>Ngay khi ký hộp đồng</span></h3>
-                    <p class="mb-3">Miễn phí thiết kế kiến trúc</p>
-                    <p>Báo giá tham khảo. Có thể thay đổi theo nhu cầu khách hàng</p>
+            {{--<div class="col-md-3 text-center">--}}
+                {{--<div class="bao-gia border">--}}
+                    {{--<h4>THIẾT KẾ</h4>--}}
+                    {{--<h3>MIỄN PHÍ<br>--}}
+                        {{--<span>Ngay khi ký hộp đồng</span></h3>--}}
+                    {{--<p class="mb-3">Miễn phí thiết kế kiến trúc</p>--}}
+                    {{--<p>Báo giá tham khảo. Có thể thay đổi theo nhu cầu khách hàng</p>--}}
 
-                    <div class="mt-4 mb-4">
-                        <a href="{{URL::asset('bao-gia.html')}}">BẢNG BÁO GIÁ</a>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-3 text-center">
+                    {{--<div class="mt-4 mb-4">--}}
+                        {{--<a href="{{URL::asset('bao-gia.html')}}">BẢNG BÁO GIÁ</a>--}}
+                    {{--</div>--}}
+                {{--</div>--}}
+            {{--</div>--}}
+            <div class="col-md-4 text-center">
                 <div class="bao-gia border">
                     <h4>SỬA CHỮA NHÀ</h4>
-                    <h3>1.200.000<span>/M<sup>2</sup></span><br>
-                        <span>(Giá chỉ từ)</span></h3>
+                    <h3>Báo Giá Trọn Gói<br>
+                        <span>(Cam Kết Không Phát Sinh Chi Phí)</span></h3>
                     <p class="mb-3">Miễn phí thiết kế kiến trúc</p>
                     <p>Báo giá tham khảo. Có thể thay đổi theo nhu cầu khách hàng</p>
 
                     <div class="mt-4 mb-4">
-                        <a href="{{URL::asset('bao-gia.html')}}">BẢNG BÁO GIÁ</a>
+                        <a href="{{URL::to('dich-vu/son-nuoc')}}">BẢNG BÁO GIÁ</a>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 text-center">
+            <div class="col-md-4 text-center">
                 <div class="bao-gia border">
                     <h4>CHỐNG THẤM</h4>
                     <h3>0909.353.055<br>
                         <span>(Gọi chúng tôi)</span></h3>
-                    <p class="mb-3">Miễn phí thiết kế kiến trúc</p>
-                    <p>Báo giá tham khảo. Có thể thay đổi theo nhu cầu khách hàng</p>
+                    <p class="mb-3">Chống Thấm Nhà Vệ Sinh, Chống Thấm Tường</p>
+                    <p>Chống Thấm Sản Mái, Tầng Hầm, Bể Nước, Bê Tông</p>
 
                     <div class="mt-4 mb-4">
-                        <a href="{{URL::asset('bao-gia.html')}}">BẢNG BÁO GIÁ</a>
+                        <a href="{{URL::to('dich-vu/chong-tham')}}">BẢNG BÁO GIÁ</a>
                     </div>
                 </div>
             </div>
-            <div class="col-md-3 text-center">
+            <div class="col-md-4 text-center">
                 <div class="bao-gia border">
                     <h4>SƠN NƯỚC</h4>
                     <h3>32.000<span>/M<sup>2</sup></span><br>
                         <span>(Giá chỉ từ)</span></h3>
-                    <p class="mb-3">Miễn phí thiết kế kiến trúc</p>
+                    <p class="mb-3">Miễn phí xử lý vết nứt, bong tróc</p>
                     <p>Báo giá tham khảo. Có thể thay đổi theo nhu cầu khách hàng</p>
 
                     <div class="mt-4 mb-4">
-                        <a href="{{URL::asset('bao-gia.html')}}">BẢNG BÁO GIÁ</a>
+                        <a href="{{URL::to('dich-vu/son-nuoc')}}">BẢNG BÁO GIÁ</a>
                     </div>
                 </div>
             </div>
 
             <div class="row justify-content-center">
-                <div class="col-md-3 text-center">
+                <div class="col-md-4 text-center">
                     <div class="bao-gia border">
                         <h4>TRẦN THẠCH CAO</h4>
-                        <h3>150.000<span>/M<sup>2</sup></span><br>
+                        <h3>115.000<span>/M<sup>2</sup></span><br>
                             <span>(Giá chỉ từ)</span></h3>
-                        <p class="mb-3">Miễn phí thiết kế kiến trúc</p>
-                        <p>Báo giá tham khảo. Có thể thay đổi theo nhu cầu khách hàng</p>
+                        <p class="mb-3">Giá Cạnh Tranh Và Tiết Kiệm</p>
+                        <p>Thi Công Trần Thạch Cao Chìm, Trần Thả</p>
 
                         <div class="mt-4 mb-4">
-                            <a href="{{URL::asset('bao-gia.html')}}">BẢNG BÁO GIÁ</a>
+                            <a href="{{URL::to('dich-vu/tran-thach-cao')}}">BẢNG BÁO GIÁ</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 text-center">
+                <div class="col-md-4 text-center">
                     <div class="bao-gia border">
                         <h4>SỬA CỬA SẮT</h4>
                         <h3>800.000<span>/M<sup>2</sup></span><br>
                             <span>(Giá chỉ từ)</span></h3>
-                        <p class="mb-3">Miễn phí thiết kế kiến trúc</p>
-                        <p>Báo giá tham khảo. Có thể thay đổi theo nhu cầu khách hàng</p>
+                        <p class="mb-3">Giá Cập Nật Liên Tục</p>
+                        <p>Thi Công Cửa Cổng, Cửa Sắt 2 Cánh, 4 Cánh, Cửa Sắt Sơn Tĩnh Điện, Giả Gỗ..</p>
 
                         <div class="mt-4 mb-4">
-                            <a href="{{URL::asset('bao-gia.html')}}">BẢNG BÁO GIÁ</a>
+                            <a href="{{URL::to('dich-vu/sua-cua-sat')}}">BẢNG BÁO GIÁ</a>
                         </div>
                     </div>
                 </div>
-                <div class="col-md-3 text-center">
+                <div class="col-md-4 text-center">
                     <div class="bao-gia border">
                         <h4>SỬA CỬA NHÔM, KÍNH</h4>
-                        <h3>850.000<span>/M<sup>2</sup></span><br>
+                        <h3>700.000<span>/M<sup>2</sup></span><br>
                             <span>(Giá chỉ từ)</span></h3>
-                        <p class="mb-3">Miễn phí thiết kế kiến trúc</p>
-                        <p>Báo giá tham khảo. Có thể thay đổi theo nhu cầu khách hàng</p>
+                        <p class="mb-3">Giá Cập Nật Liên Tục</p>
+                        <p>Thi Công Kính Cường Lực, Cửa Nhôm Xingfa</p>
 
                         <div class="mt-4 mb-4">
-                            <a href="{{URL::asset('bao-gia.html')}}">BẢNG BÁO GIÁ</a>
+                            <a href="{{URL::to('dich-vu/sua-cua-nhom-kinh')}}">BẢNG BÁO GIÁ</a>
                         </div>
                     </div>
                 </div>
